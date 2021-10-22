edb76a422aa1811a0bffd360be8a17bbffb5713b
Hannan Fakhrul Hakim <58686484+hannanfh@users.noreply.github.com>
2021-10-22 14:51:11 +0700

    menyelesaikan video 2 database dan 3 post model pertemuan ke 6

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,9 +1,8 @@
 @extends('layouts.main')
 
 @section('container')
-    <h2>{{ $post["title"] }}</h2>
-    {{-- <h5>{{ $post["author"] }}</h5> --}}
-    <p>{{ $post["body"] }}</p>
+    <h2>{{ $post->title }}</h2>
+    {!! $post->body !!}
 
     <a href="/posts">Back to Posts</a>
 @endsection
