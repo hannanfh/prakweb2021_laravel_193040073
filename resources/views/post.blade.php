f5646f9ea43d4915ff0947836d9cb110864707b8
Hannan Fakhrul Hakim <58686484+hannanfh@users.noreply.github.com>
2021-10-23 21:10:34 +0700

    menyelesaikan video 4 post category pertemuan ke 6

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,6 +2,9 @@
 
 @section('container')
     <h2>{{ $post->title }}</h2>
+
+   <p> By. Hannan Fakhrul Hakim in <a href="/categories/{{ $post->category->slug }}"> {{$post->category->name}}</p> </a>
+
     {!! $post->body !!}
 
     <a href="/posts">Back to Posts</a>
