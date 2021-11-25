00c97df9cd3a7d85265f07eb35f146681091e490
Hannan Fakhrul Hakim <58686484+hannanfh@users.noreply.github.com>
2021-11-25 22:10:42 +0700

    menyelesaikan video ke 17 pertemuan ke 8

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,14 @@
 // use App\Models\Post;
 // use App\Models\User;
 
-use App\Http\Controllers\DashboardController;
+
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
-use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
+// use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
+use App\Http\Controllers\DashboardPostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -58,4 +59,11 @@ Route::post('/logout', [LoginController::class, 'logout']);
 Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store']);
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
+Route::get('/dashboard', function () {
+    return view('dashboard.index');
+})->middleware('auth');
+
+
+
+Route::resource('/dashboard/posts', DashboardPostController::class)
+    ->middleware('auth');
