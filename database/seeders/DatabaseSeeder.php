c5d95d331cf0a4d5a1c73866793a6fac2f90e226
Hannan Fakhrul Hakim <58686484+hannanfh@users.noreply.github.com>
2021-11-27 15:52:21 +0700

    menyelesaikan video ke 21 upload image pertemuan ke 10

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,11 +17,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-        // User::create([
-        //     'name' => 'Hannan Fh',
-        //     'email' => '193040073@mail.unpas.ac.id',
-        //     'password' => bcrypt('12345')
-        // ]);
+        User::create([
+            'name' => 'Hannan Fh',
+            'username' => 'hannanfh',
+            'email' => 'alhafahanura@gmail.com',
+            'password' => bcrypt('12345678')
+        ]);
 
         // User::create([
         //     'name' => 'Nura Nuraeni',
@@ -47,40 +48,5 @@ class DatabaseSeeder extends Seeder
         ]);
 
         Post::factory(20)->create();
-        // Post::create([
-        //     'title' => 'Judul Pertama',
-        //     'slug' => 'judul-pertama',
-        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis autem odit incidunt blanditiis',
-        //     'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis autem odit incidunt blanditiis. Sapiente dolores vero totam nisi accusamus eaque quos incidunt, perferendis repellendus deserunt iste accusantium architecto nihil illo modi? Deserunt itaque ex at autem nulla veniam culpa earum dignissimos illum sit, expedita vel magnam deleniti asperiores. Commodi provident saepe perferendis quibusdam unde quas dolorum officiis corrupti. Neque iste itaque quasi? Doloribus officiis laborum, laboriosam ipsam sequi rerum enim ullam totam eius dolor? Debitis eos aperiam quas! Magni ipsa repudiandae, voluptate, optio animi obcaecati rem ipsum tempora, ex harum veritatis. In repellendus, voluptatum ex obcaecati fugit est culpa fuga?',
-        //     'category_id' => 1,
-        //     'user_id' => 1
-        // ]);
-
-        // Post::create([
-        //     'title' => 'Judul Kedua',
-        //     'slug' => 'judul-kedua',
-        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis autem odit incidunt blanditiis',
-        //     'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis autem odit incidunt blanditiis. Sapiente dolores vero totam nisi accusamus eaque quos incidunt, perferendis repellendus deserunt iste accusantium architecto nihil illo modi? Deserunt itaque ex at autem nulla veniam culpa earum dignissimos illum sit, expedita vel magnam deleniti asperiores. Commodi provident saepe perferendis quibusdam unde quas dolorum officiis corrupti. Neque iste itaque quasi? Doloribus officiis laborum, laboriosam ipsam sequi rerum enim ullam totam eius dolor? Debitis eos aperiam quas! Magni ipsa repudiandae, voluptate, optio animi obcaecati rem ipsum tempora, ex harum veritatis. In repellendus, voluptatum ex obcaecati fugit est culpa fuga?',
-        //     'category_id' => 1,
-        //     'user_id' => 1
-        // ]);
-
-        // Post::create([
-        //     'title' => 'Judul Ketiga',
-        //     'slug' => 'judul-ketiga',
-        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis autem odit incidunt blanditiis',
-        //     'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis autem odit incidunt blanditiis. Sapiente dolores vero totam nisi accusamus eaque quos incidunt, perferendis repellendus deserunt iste accusantium architecto nihil illo modi? Deserunt itaque ex at autem nulla veniam culpa earum dignissimos illum sit, expedita vel magnam deleniti asperiores. Commodi provident saepe perferendis quibusdam unde quas dolorum officiis corrupti. Neque iste itaque quasi? Doloribus officiis laborum, laboriosam ipsam sequi rerum enim ullam totam eius dolor? Debitis eos aperiam quas! Magni ipsa repudiandae, voluptate, optio animi obcaecati rem ipsum tempora, ex harum veritatis. In repellendus, voluptatum ex obcaecati fugit est culpa fuga?',
-        //     'category_id' => 2,
-        //     'user_id' => 1
-        // ]);
-
-        // Post::create([
-        //     'title' => 'Judul Keempat',
-        //     'slug' => 'judul-keempat',
-        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis autem odit incidunt blanditiis',
-        //     'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis autem odit incidunt blanditiis. Sapiente dolores vero totam nisi accusamus eaque quos incidunt, perferendis repellendus deserunt iste accusantium architecto nihil illo modi? Deserunt itaque ex at autem nulla veniam culpa earum dignissimos illum sit, expedita vel magnam deleniti asperiores. Commodi provident saepe perferendis quibusdam unde quas dolorum officiis corrupti. Neque iste itaque quasi? Doloribus officiis laborum, laboriosam ipsam sequi rerum enim ullam totam eius dolor? Debitis eos aperiam quas! Magni ipsa repudiandae, voluptate, optio animi obcaecati rem ipsum tempora, ex harum veritatis. In repellendus, voluptatum ex obcaecati fugit est culpa fuga?',
-        //     'category_id' => 2,
-        //     'user_id' => 2
-        // ]);
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -9,7 +9,13 @@
 
             <p> By. <a href="/posts?author={{ $post->author->username }}" class="text-decoration-none"> {{ $post->author->name }} </a> in <a href="/posts?category={{ $post->category->slug }}" class="text-decoration-none"> {{$post->category->name}}</p> </a>
          
-            <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{$post->category->name}}" class="img-fluid">
+            @if ($post->image)
+            <div style="max-height: 350px; overflow:hidden;">
+                <img src="{{ asset('storage/' . $post->image) }}" alt="{{ $post->category->name }}" class="img-fluid mt-3">
+            </div>
+        @else
+        <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid">
+        @endif
             <article class="my-3 fs-5">
              {!! $post->body !!}
             </article>
@@ -20,3 +26,4 @@
 </div>
  
 @endsection
+
